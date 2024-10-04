9fb93826bafb5541684389b742655f297d0c8cc0
Kyle Stewart <kgstew@gmail.com>
2024-10-03 18:35:31 -0600

    Update logo svg

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-            <div className="mr-3">
+            <div className="mr-4">
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
