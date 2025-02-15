b536cba531f9512bdc2d9d02a3e4e15128690147
Kyle Stewart <kgstew@gmail.com>
2025-02-15 12:05:56 -0700

    Update projects and images

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,17 @@
 const projectsData = [
   {
+    title: 'Fable Bound',
+    description: `Fable Bound is a narrative journey into the life a young female viking. It's a wooden ship built with a computer controlled system for movement, lights and sound that invites you to climb aboard and experience your own heroes journey.`,
+    imgSrc: '/static/images/projects/fable_bound.jpg',
+    href: 'https://www.buildtostrike.org',
+  },
+  {
+    title: 'League of Creative Technologists',
+    description: `A monthly meetup for technical artists to gather, discuss projects, find collaborations and socialize. We also host regular workshops on incorporating technology into art.`,
+    imgSrc: '/static/images/projects/loct.jpg',
+    href: 'https://www.facebook.com/groups/177597161701995/',
+  },
+  {
     title: 'denhac',
     description: `denhac provides space and tools for makers and creatives of all types. The space contains a woodshop, metalshop; laser cutting, 3d printing, cnc and textiles areas, an electronics room and workshop space for member use.`,
     imgSrc: '/static/images/projects/denhac.jpg',
@@ -10,12 +22,6 @@ const projectsData = [
     description: `The largest directory of spaces, resources, support and information for creatives in Colorado. Find your community.`,
     imgSrc: '/static/images/projects/colorado-maker-directory.jpg',
     href: 'https://iamamaker.co',
-  },
-  {
-    title: 'League of Creative Technologists',
-    description: `A monthly meetup for technical artists to gather, discuss projects, find collaborations and socialize. We also host regular workshops on incorporating technology into art.`,
-    imgSrc: '/static/images/projects/techart.jpg',
-    href: 'https://www.facebook.com/groups/177597161701995/',
   },
 ]
 export default projectsData
